e95b816c373c4136a2d27fda840a7dc83259495e
Allie <alliemkelley@gmail.com>
2018-03-28 11:44:17 -0700

    made changes to index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Agate Dairy Farming' });
+  res.render('index', { title: 'Agate Cheese Making' });
 });
 
 module.exports = router;
